72282386e770785018a264e429a2658b0d5d3aaf
Ben Whitehead <benw@li341-44.members.linode.com>
2012-02-21 12:38:09 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'ae84545303181c105fcc6e1903d11f4572b4b6f03e9ac945d03917fc726367c171f7f153c4552e8a448cfed590ff1a382227595293873a40bf25d615e9f62984'
